8ce2a9f68dab9679347efaeefb221cca84fbbb11
anitap26 <munozallueana@gmail.com>
2024-12-17 22:18:45 +0100

    Update api.py

--- app/api.py
+++ app/api.py
@@ -43,3 +43,25 @@ def multiply(op_1, op_2):
         # Si ocurre un error de tipo, devolver un error 400 (Bad Request)
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
+@api_application.route("/calc/divide/<op_1>/<op_2>", methods=["GET"])
+def divide(op_1, op_2):
+    try:
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        result = num_1 / num_2  # Esto lanzará un ZeroDivisionError si el divisor es cero
+        return ("{}".format(result), http.client.OK, HEADERS)
+    
+    except ZeroDivisionError:
+        # Si el divisor es cero, devolver un error 406 (Not Acceptable)
+        return ("Cannot divide by zero", http.client.NOT_ACCEPTABLE, HEADERS)
+    
+    except ValueError as e:
+        # Si ocurre un error de valor, devolver un error 400 (Bad Request)
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+    
+    except TypeError as e:
+        # Si ocurre un error de tipo, devolver un error 400 (Bad Request)
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+    
+    except Exception as e:
+        # Captura cualquier otro error inesperado y devuelve un error 500 (Internal Server Error)
+        return (f"Internal server error: {str(e)}", http.client.INTERNAL_SERVER_ERROR, HEADERS)
